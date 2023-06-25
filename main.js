25eafc28934cefc47c8f293eeb9fd24e67180195
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-06-25 15:45:23 +0200

    need the image

---
+++ main.js
@@ -0,0 +1,7 @@
+const blast = document.getElementById('blast')
+const rocketShip = document.querySelector('.rocket-ship')
+
+blast.addEventListener('click',()=>{
+  rocketShip.style.display = 'flex'
+})
+
