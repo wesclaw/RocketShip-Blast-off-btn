2601032048c6ff6717f85ddabb65cd98a0cd27a2
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-06-25 15:51:02 +0200

    finished

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const blast = document.getElementById('blast')
 const rocketShip = document.querySelector('.rocket-ship')
+const span = document.querySelector('span')
 
 blast.addEventListener('click',()=>{
   rocketShip.style.display = 'flex'
